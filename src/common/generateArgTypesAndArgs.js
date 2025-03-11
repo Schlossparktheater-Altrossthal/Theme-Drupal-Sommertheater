05d57995919613e9b3687478da59555d8acc7b38
dantedubonc <dante.dubon@mediacurrent.com>
2025-03-11 14:31:30 -0600

    DRU-17: Formatted

--- src/common/generateArgTypesAndArgs.js
+++ src/common/generateArgTypesAndArgs.js
@@ -1,63 +1,61 @@
 const generateControlType = (property) => {
-  if(property.enum) {
+  if (property.enum) {
     return {
-      type: 'select',
+      type: "select",
       options: property.enum,
     };
   }
-  if(property.type === 'array') {
-
+  if (property.type === "array") {
     return {
-      type: 'object',
+      type: "object",
     };
   }
 
-  if(Array.isArray(property.type)) {
+  if (Array.isArray(property.type)) {
     return {
-      type: 'text',
+      type: "text",
     };
   }
-  
-    return {
-      type:property.type,
-    };
-  
-  
-}
+
+  return {
+    type: property.type,
+  };
+};
 
 const generateArgTypesAndArgs = (parsedMetadata) => {
-    // Parse metadata if it's a string
-    const argTypes = {};
-    const args = {};
-  
-    if (!parsedMetadata.props || !parsedMetadata.props.properties) {
-      console.error('YAML metadata is missing the "props.properties" field.');
-      return { argTypes, args };
-    }
-    const properties = parsedMetadata.props.properties;
-  
-    Object.keys(properties).forEach((key) => {
-      const property = properties[key];
-  
-      // Infer the control type based on the property type.
-      const control = generateControlType(property);
-     
-  
-      // Build the argTypes entry.
-      argTypes[key] = {
-         ...control,
-        description: property.description,
-        table: {
-          type: { summary: property.type },
-        },
-      };
-  
-      // Set the default value from the first example, if available.
-      args[key] =
-        property.examples && property.examples.length > 0 ? property.examples[0] : '';
-    });
-  
-    return { argTypes, args };
-  };
+  // Parse metadata if it's a string
+  const argTypes = {};
+  const args = {};
 
-  export default generateArgTypesAndArgs;
+  if (!parsedMetadata.props || !parsedMetadata.props.properties) {
+    console.error('YAML metadata is missing the "props.properties" field.');
+    return { argTypes, args };
+  }
+  const properties = parsedMetadata.props.properties;
+
+  Object.keys(properties).forEach((key) => {
+    const property = properties[key];
+
+    // Infer the control type based on the property type.
+    const control = generateControlType(property);
+
+    // Build the argTypes entry.
+    argTypes[key] = {
+      ...control,
+      description: property.description,
+      table: {
+        type: { summary: property.type },
+      },
+    };
+
+    // Set the default value from the first example, if available.
+    args[key] =
+      property.examples && property.examples.length > 0
+        ? property.examples[0]
+        : "";
+  });
+
+  return { argTypes, args };
+};
+
+export default generateArgTypesAndArgs;
