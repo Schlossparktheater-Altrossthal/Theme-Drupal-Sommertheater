b84ee2e7a12601b2aa8313f9dd5537629d5485e0
dantedubonc <dante.dubon@mediacurrent.com>
2025-04-09 21:00:29 -0600

    DRU-76: Removing unnecesary code

--- .storybook/main.js
+++ .storybook/main.js
@@ -25,17 +25,7 @@ const config = {
   // Configure Vite for Storybook
   viteFinal: (config, { configType }) => {
     console.log(`[storybook] Configuring Vite for Storybook (${configType})`);
-    
-    // Only run this step during build-storybook (not during development)
-    if (configType === 'PRODUCTION') {
-      console.log('[storybook] Executing custom build step for production build');
-      // Add your custom build step here
-      // For example, you could:
-      // - Import and run functions from your custom script
-      // - Process additional assets
-      // - Modify the build configuration
-    }
-    
+  
     return config;
   },
 };
