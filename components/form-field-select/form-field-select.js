f2a674f6a1c5a2ea4841bbc78c1b6e30850eb654
Felipe Barone <felipe.barone@mediacurrent.com>
2025-06-10 09:11:52 -0300

    DRU-112: Select fixes

---
+++ components/form-field-select/form-field-select.js
@@ -0,0 +1,34 @@
+import { ComponentType, ComponentInstance } from '../../src/common/component.js';
+class FormFieldSelect extends ComponentInstance {
+
+  init() {
+    let select = this.el.querySelector('.form-element--type-select');
+    let clearButton = this.el.querySelector('.clear-select');
+
+    if (!select || !clearButton) {
+      console.warn('FormFieldSelect: Missing required elements.');
+      return;
+    }
+
+    function updateopenButton() {
+      if (select.value) {
+        clearButton.classList.remove('hidden');
+      } else {
+        clearButton.classList.add('hidden');
+      }
+    }
+    select.addEventListener('change', updateopenButton);
+    updateopenButton();
+
+    clearButton.addEventListener('click', () => {
+      select.value = '';
+      select.dispatchEvent(new Event('change'));
+    });
+  }
+}
+
+new ComponentType(
+  FormFieldSelect,
+  'formFieldSelect',
+  '.form-type-select'
+);
