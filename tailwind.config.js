c226e26916efea459a99e21eb19784241d5a4e84
Jason Thompson <jason.thompson@mediacurrent.com>
2025-06-17 11:02:50 -0500

    DRU-124: wip

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+
+  safelist: [
+    'grid-cols-[33.3%_66.7%]',
+    'grid-cols-[66.7%_33.3%]',
+    'md:grid-cols-[33.3%_66.7%]',
+    'md:grid-cols-[66.7%_33.3%]',
+    'w-[33.3%]',
+    'w-[66.7%]',
+    // add any other classes you want to force to keep here
+  ],
+
+};
