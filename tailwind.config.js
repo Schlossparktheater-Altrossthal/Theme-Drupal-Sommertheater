b1caf81d8b0436edfb45d89f9ac1c0bf9f957dde
Jason Thompson <jason.thompson@mediacurrent.com>
2025-06-18 17:45:56 -0500

    DRU-124: Column component.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,39 @@
 export default {
 
   safelist: [
-    'grid-cols-[33.3%_66.7%]',
-    'grid-cols-[66.7%_33.3%]',
-    'md:grid-cols-[33.3%_66.7%]',
-    'md:grid-cols-[66.7%_33.3%]',
-    'w-[33.3%]',
-    'w-[66.7%]',
+    'grid-cols-[1fr]',
+    'grid-cols-[1fr_2fr]',
+    'grid-cols-[2fr_1fr]',
+    'grid-cols-[1fr_1fr]',
+    'grid-cols-[2fr_3fr]',
+    'grid-cols-[3fr_2fr]',
+    'grid-cols-[1fr_3fr]',
+    'grid-cols-[3fr_1fr]',
+    'grid-cols-[1fr_1fr_2fr]',
+    'grid-cols-[2fr_1fr_1fr]',
+    'grid-cols-[1fr_2fr_1fr]',
+    'grid-cols-[1fr_1fr_1fr]',
+    'grid-cols-[1fr_1fr_1fr_1fr]',
+    'md:grid-cols-[1fr]',
+    'md:grid-cols-[1fr_2fr]',
+    'md:grid-cols-[2fr_1fr]',
+    'md:grid-cols-[1fr_1fr]', // 50 50
+    'md:grid-cols-[2fr_3fr]',
+    'md:grid-cols-[3fr_2fr]',
+    'md:grid-cols-[1fr_3fr]',
+    'md:grid-cols-[3fr_1fr]',
+    'md:grid-cols-[1fr_1fr_2fr]',
+    'md:grid-cols-[2fr_1fr_1fr]',
+    'md:grid-cols-[1fr_2fr_1fr]',
+    'md:grid-cols-[1fr_1fr_1fr]',
+    'md:grid-cols-[1fr_1fr_1fr_1fr]',
+    'max-w-9/10',
+    'max-w-8/10',
+    'max-w-3/4',
+    'max-w-1/2',
     // add any other classes you want to force to keep here
   ],
 
 };
+
+
