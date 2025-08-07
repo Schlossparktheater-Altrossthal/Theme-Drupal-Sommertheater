739b59cd5f38b6e4237bb7e51b3f0ec08cf73c55
Adam G-H <32250-phenaproxima@users.noreply.drupalcode.org>
2025-08-07 14:00:46 +0000

    Issue #3539895 by phenaproxima, hestenet, lanny heidbreder, galactus86: Support the Phosphor icon library used by core

---
+++ tests/src/Kernel/IconsTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\mercury\Kernel;
+
+use Drupal\Core\Extension\ThemeExtensionList;
+use Drupal\Core\Extension\ThemeInstallerInterface;
+use Drupal\Core\Theme\Icon\Plugin\IconPackManagerInterface;
+use Drupal\KernelTests\KernelTestBase;
+use PHPUnit\Framework\Attributes\Group;
+
+/**
+ * Tests that all of Mercury's icons are discoverable.
+ */
+#[Group('mercury')]
+final class IconsTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['twig_field_value', 'twig_tweak'];
+
+  /**
+   * Tests that all Mercury icons are discoverable.
+   */
+  public function testIconsAreDiscoverable(): void {
+    $this->container->get(ThemeInstallerInterface::class)->install(['mercury']);
+
+    $icons = $this->container->get(IconPackManagerInterface::class)
+      ->getIcons();
+
+    $icon_dir = implode('/', [
+      $this->getDrupalRoot(),
+      $this->container->get(ThemeExtensionList::class)->getPath('mercury'),
+      'icons',
+    ]);
+    $dir = opendir($icon_dir);
+    $this->assertIsResource($dir);
+
+    while ($item = readdir($dir)) {
+      if ($item === '.' || $item === '..') {
+        continue;
+      }
+      $this->assertStringEndsWith('.svg', $item);
+      $id = 'phosphor:' . substr($item, 0, -4);
+      $this->assertSame($icon_dir . '/' . $item, $icons[$id]['absolute_path']);
+    }
+    closedir($dir);
+  }
+
+}
