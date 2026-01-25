63baf8430316943f081d7b0f1728a253b6e10fa3
Pamela Barone <31375-pameeela@users.noreply.drupalcode.org>
2026-01-25 03:19:36 +0000

    feat: #3562157 Add basic styling for search recipe
    By: pameeela
    By: phenaproxima

--- src/Hook/ThemeHooks.php
+++ src/Hook/ThemeHooks.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace Drupal\mercury\Hook;
 
 use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Breadcrumb\ChainBreadcrumbBuilderInterface;
+use Drupal\Core\Controller\TitleResolverInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Extension\ThemeExtensionList;
 use Drupal\Core\Extension\ThemeSettingsProvider;
 use Drupal\Core\Hook\Attribute\Hook;
 use Drupal\mercury\RenderCallbacks;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\node\NodeInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -26,6 +32,11 @@ final class ThemeHooks {
     private readonly ThemeSettingsProvider $themeSettings,
     private readonly RequestStack $requestStack,
     private readonly ThemeExtensionList $themeList,
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+    private readonly RouteMatchInterface $routeMatch,
+    private readonly TitleResolverInterface $titleResolver,
+    private readonly ChainBreadcrumbBuilderInterface $breadcrumb,
+    private readonly ModuleHandlerInterface $moduleHandler,
     #[Autowire(param: 'app.root')] string $appRoot,
   ) {
     self::$appRoot ??= $appRoot;
@@ -101,6 +112,47 @@ final class ThemeHooks {
     $variables['scheme'] = $this->themeSettings->getSetting('scheme');
     // Get the theme base path for font preloading.
     $variables['mercury_path'] = $this->requestStack->getCurrentRequest()->getBasePath() . '/' . $this->themeList->getPath('mercury');
+  }
+
+  /**
+   * Implements template_preprocess_page().
+   */
+  #[Hook('preprocess_page')]
+  public function preprocessPage(array &$variables): void {
+    // @see \Drupal\Core\Block\Plugin\Block\PageTitleBlock::build()
+    $variables['title'] = [
+      '#type' => 'page_title',
+      '#title' => $variables['page']['#title'] ?? $this->titleResolver->getTitle(
+        $this->requestStack->getCurrentRequest(),
+        $this->routeMatch->getRouteObject(),
+      ),
+    ];
+
+    // @see \Drupal\system\Plugin\Block\SystemBreadcrumbBlock::build()
+    $variables['breadcrumb'] = $this->breadcrumb->build($this->routeMatch)
+      ->toRenderable();
+
+    $route_name = $this->routeMatch->getRouteName();
+    if ($route_name === 'entity.canvas_page.canonical' || str_starts_with($this->routeMatch->getRouteObject()?->getPath() ?? '', '/canvas/')) {
+      $variables['rendered_by_canvas'] = TRUE;
+    }
+    elseif ($route_name === 'entity.node.canonical' && $this->moduleHandler->moduleExists('canvas')) {
+      $node = $this->routeMatch->getParameter('node');
+      assert($node instanceof NodeInterface);
+
+      $variables['rendered_by_canvas'] = (bool) $this->entityTypeManager->getStorage('content_template')
+        ->getQuery()
+        ->accessCheck(FALSE)
+        ->count()
+        ->condition('content_entity_type_id', 'node')
+        ->condition('content_entity_type_bundle', $node->getType())
+        ->condition('content_entity_type_view_mode', 'full')
+        ->condition('status', TRUE)
+        ->execute();
+    }
+    else {
+      $variables['rendered_by_canvas'] = FALSE;
+    }
   }
 
 }
